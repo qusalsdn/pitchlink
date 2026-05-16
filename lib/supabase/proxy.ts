461aeb59b4a03a4a89fd768a858c4be105d306df
변민우 <yja05092@gmail.com>
2026-05-16 17:00:00 +0900

    feat: 프로젝트 재설정

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -8,23 +8,19 @@ export const createClient = (request: NextRequest) => {
 
   const supabase = createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
       cookies: {
         getAll() {
           return request.cookies.getAll();
         },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) =>
-            request.cookies.set(name, value)
-          );
+          cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
           supabaseResponse = NextResponse.next({ request });
-          cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
-          );
+          cookiesToSet.forEach(({ name, value, options }) => supabaseResponse.cookies.set(name, value, options));
         },
       },
-    }
+    },
   );
 
   return { supabase, supabaseResponse };
